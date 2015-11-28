3459b447a18586e0f98304a0023fdb194eac11db
Birla Rahul <c2birlar@cdf.toronto.edu>
2015-11-28 15:12:19 -0500

    trying to pull

--- felix_a4/server.js
+++ felix_a4/server.js
@@ -1,140 +1,80 @@
-// Load modules
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var http = require('http');
-var useragent = require('express-useragent');
-var expressValidator = require('express-validator');
-var passport = require('passport');
-var session = require('express-session');
 var mongoose = require('mongoose');
+var logger = require('morgan');
+var fs = require('fs');
+var _ = require('underscore');
+var device = require('express-device');
+var requestIp = require('request-ip');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var mongoStore = require('connect-mongo')(session);
 
-// Load routes
-var routes = require('./routes/index');
-var users = require('./routes/users');
-var login = require('./routes/login');
-var signup = require('./routes/signup');
-var logout = require('./routes/logout');
-
+var port = process.env.PORT || 3000;
 var app = express();
 
-// Configure view engine
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+var dbUrl = process.env.MONGOLAB_URI || 'mongodb://localhost/a5';
+mongoose.connect(dbUrl);
 
-// Configure some modules
-app.use(useragent.express());
-app.use(expressValidator());
-app.use(logger('dev'));
+// models loading
+var models_path = __dirname + '/app/models';
+var walk = function(path) {
+  fs
+    .readdirSync(path)
+    .forEach(function(file) {
+      var newPath = path + '/' + file;
+      var stat = fs.statSync(newPath);
+
+      if (stat.isFile()) {
+        if (/(.*)\.(js|coffee)/.test(file)) {
+          require(newPath);
+        }
+      }
+      else if (stat.isDirectory()) {
+        walk(newPath);
+      }
+    })
+}
+walk(models_path);
+
+
+app.set('views','./app/views/pages');  //views files
+app.set('view engine','jade');//set templete
+app.use(express.static(path.join(__dirname,'public')));
+app.locals.moment = require('moment');
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// parse application/json
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-// Configure Passport
-app.use(session({ secret: '9032ke0dke09fksd9fsdf90sf' }));
-app.use(passport.initialize());
-app.use(passport.session());
-require('./config/passport')(passport);
+app.use(session({
+	secret:'imooc',
+	resave: false,
+	saveUninitialized: true,
+	store: new mongoStore({
+		url: dbUrl,
+		collection: 'sessions'
+	})
+}));
 
-// Configure routes
-app.use('/', routes);
-app.use('/users', users);
-app.use('/login', login);
-app.use('/signup', signup);
-app.use('/logout', logout);
+app.set('view options', { layout: false });
+app.use(device.capture());
 
-// 404 handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
+var env = process.env.NODE_ENV || 'development';
 
-// Error handler for development
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
+if ('development' === env) {
+	//print
+	app.set('showStackError',true);
+	app.use(logger(':method :url :status'));
+	app.locals.pretty = true;
+	mongoose.set('debug',true);
 }
 
-// Error handler for production
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
 
-// Setup server
-var port = normalizePort(process.env.PORT || '3000');
-app.set('port', port);
+require('./config/router')(app);
 
-var server = http.createServer(app);
+app.listen(port);	//starts here!!!
 
-server.listen(port);
-server.on('error', onError);
-server.on('listening', onListening);
-
-function normalizePort(val) {
-    var port = parseInt(val, 10);
-
-    if (isNaN(port)) {
-        // named pipe
-        return val;
-    }
-
-    if (port >= 0) {
-        // port number
-        return port;
-    }
-
-    return false;
-}
-
-function onError(error) {
-    if (error.syscall !== 'listen') {
-        throw error;
-    }
-
-    var bind = typeof port === 'string'
-        ? 'Pipe ' + port
-        : 'Port ' + port;
-
-    switch (error.code) {
-        case 'EACCES':
-            console.error(bind + ' requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-        throw error;
-    }
-}
-
-function onListening() {
-    var addr = server.address();
-    var bind = typeof addr === 'string'
-        ? 'pipe ' + addr
-        : 'port ' + addr.port;
-        console.log('Listening on ' + bind);
-}
-
-// Setup Mongoose
-mongoose.connect('mongodb://localhost/g4felix_csc309_a4');
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function (callback) {
-    // Do nothing.
-});
+console.log('Started on port:' + port);
