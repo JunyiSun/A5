d878946cc9b3310297a4a614ade64cd3d55b4984
Felix Mo <felix@felixmo.com>
2015-11-28 13:12:35 -0500

    Don't repeat textbook ranking logic

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,6 +2,7 @@ var Textbook = require('../models/textbook');
 var Subject = require('../models/subject');
 var View = require('../models/view');
 var async = require('async');
+var recommendations = require('../recommendations');
 
 // index page
 exports.index = function(req, res) {
@@ -70,53 +71,7 @@ exports.search = function(req, res) {
         }
         var results = textbooks.slice(index, index + count)
 
-		// Store ranking info in 'rankings' with each ranking represented as an array with the textbook as the first element and the weight as the second value
-		var rankings = [];
-		results.forEach(function(result) {
-			rankings.push([result, 0]);
-		});
-
-		// Prepare weight calculation tasks
-		var weighingTasks = []
-		rankings.forEach(function(pair) {
-			weighingTasks.push(function(callback) {
-				var textbook = pair[0];
-
-				// Look for views of the textbook and of the subject of the textbook by the user
-				View.find()
-				.and([
-					{ $or: [ { subject: textbook.subject }, { textbook: textbook } ] },
-					{ user: suser },
-				])
-				.exec(function (err, viewResults) {
-					if (err) {
-						console.log(err);
-					}
-					else {
-						// Weight of the result should be the total number of views for the textbook and its subject by the user
-						pair[1] += viewResults.reduce(function(previousView, currentView, currentIndex, array) {
-							return previousView.views + currentView.views;
-						});
-					}
-					callback()
-				});
-			});
-		});
-
-		// Run tasks to calculate weighting
-		async.parallel(weighingTasks, function() {
-			// This is run after the weighting is complete
-
-			// Rank result by weight, in descending order (heaviest first)
-			rankings.sort(function(first, second) {
-				return second[1] - first[1];
-			});
-
-			// Get ordered array of textbooks from ranking info
-			var rankedItems = rankings.map(function(pair) {
-				return pair[0];
-			});
-
+		recommendations.rankTextbooks(suser, results, function(rankedTextbooks) {
 			res.render('subject_detail', {
 			  title: 'Search Result Page',
 			  keyword: q,
@@ -124,8 +79,8 @@ exports.search = function(req, res) {
 			  query: 'q=' + q,
 			  totalPage: Math.ceil(textbooks.length / count),
 						sessionuser: suser,
-			  textbooks: rankedItems
-		  });
+			  textbooks: rankedTextbooks
+			});
 		});
 	});
   }
