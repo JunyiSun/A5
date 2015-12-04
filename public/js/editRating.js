7351df244e71309d770bb5f38c243f6a94601641
Birla Rahul <c2birlar@cdf.toronto.edu>
2015-12-04 12:51:21 -0500

    rate button finished

--- public/js/editRating.js
+++ public/js/editRating.js
@@ -4,33 +4,37 @@ $(function(){
 	
 	$('.increaseRating').click(function(e){
 		var target = $(e.target);
-		var id = target.data('id');  
-
+		var id = target.data('id');
+		var rate_section = $('.rate');
+		console.log("nice");
 		$.ajax({
 			type : 'PUT',
 			url : '/rating?id=' + id
 		})
+		//console.log("man");
 		.done(function(res){
-            		if (res.success === 1 && tr){
-                	target.remove();
 			
-            	}
+			
+            		if (res.success === 1 && rate_section){
+				rate_section.remove();
+            		}
         	});
+		//console.log(res.success);
 		
 	});
-	//get up rate button
+	//get down rate button
 	
 	$('.decreaseRating').click(function(e){
 		var target = $(e.target);
 		var id = target.data('id');  
-
+		var rate_section = $('.rate');
 		$.ajax({
 			type : 'PUT',
 			url : '/ratingdown?id=' + id
 		})
 		.done(function(res){
-            		if (res.success === 1 && tr){
-                	target.remove();
+            		if (res.success === 1 && rate_section){
+				rate_section.remove();
 			
             	}
         	});
