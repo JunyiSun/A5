97d626184469e940a932c53a110f422a9f3ab7d4
Felix Mo <felix@felixmo.com>
2015-12-05 01:23:28 -0500

    Compress avatar image

--- app/schemas/user.js
+++ app/schemas/user.js
@@ -53,7 +53,7 @@ UserSchema.pre('save',function(next){
 	if(this.isNew){
 		this.meta.createAt = this.meta.updateAt = Date.now();
 		this.name = this.email;
-		this.image = "gravatar.jpeg";
+		this.image = "gravatar.png";
 	}
 	else{
 		this.meta.updateAt = Date.now();
